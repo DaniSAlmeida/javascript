006c1e76d81cf7f3522eb1cf66f9e943226e532e
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-20 17:58:50 +0100

    funções em js

---
+++ aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parimpar(n) {
+   if (n%2 == 0) {
+    return 'Par!'
+   } else {
+    return 'Ímpar'
+   }
+}
+//console.log(parimpar(4)) dá no mesmo que o que está embaixo
+var res = parimpar(102)
+console.log(res)
